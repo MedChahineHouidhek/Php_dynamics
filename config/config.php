01048d7985e4da9196610d32db4150cf5e820bad
MedChahineHouidhek <chahinehouidhek2@gmail.com>
2024-02-16 16:13:45 -0500

    Tache

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 <?php
 // config/config.php
 
-define('AZURE_AD_TENANT_ID', 'f5da366b-e769-4b9f-92c1-9d15dbb5df41');
-define('AZURE_AD_CLIENT_ID', 'c97f5553-0205-4f55-975c-059bec92e7e2');
-define('AZURE_AD_CLIENT_SECRET', '.qp8Q~Cn-pYFQpV79venIAuUKVC3m_qscyceVaEy');
-define('DYNAMICS_365_RESOURCE_URL', 'https://anfv1.crm.dynamics.com');
+define('AZURE_AD_TENANT_ID', 'tenantId');
+define('AZURE_AD_CLIENT_ID', 'ClientId');
+define('AZURE_AD_CLIENT_SECRET', 'SecretId');
+define('DYNAMICS_365_RESOURCE_URL', 'lienCorrect');
